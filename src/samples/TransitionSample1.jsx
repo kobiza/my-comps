3025056af6c4f6baae8c49b6fe6015cddc5afb87
kobiz <kobiz@wix.com>
2020-12-20 23:14:25 +0200

    working

--- src/samples/TransitionSample1.jsx
+++ src/samples/TransitionSample1.jsx
@@ -1,26 +1,29 @@
 import './TransitionSample1.css';
 import {useState, useRef, useEffect} from 'react';
 import classnames from 'classnames'
-// import usePrevious from '../customHooks/usePrevious';
 
-const isUndefined = (value) => typeof value === 'undefined'
 
 function TransitionSample1() {
     const [currentIndex, setCurrentIndex] = useState(0)
     const [transitionFlags, setTransitionFlags] = useState({
         enter: -1,
-        'enter-active': -1,
+        'enterActive': -1,
         exit: -1,
-        'exit-active': -1,
+        'exitActive': -1,
     })
-    // const prevIndex = usePrevious(currentIndex)
-    // const prevIndexRef = useRef();
-    // console.log('------ render ------')
-    // useEffect(() => {
-    //     console.log('------ useEffect ------')
-    //     prevIndexRef.current = currentIndex;
-    // });
-    // const prevIndex = prevIndexRef.current;
+
+    // 0 - current
+
+    //nav to 1
+    // ---
+    // 0 - exit
+    // 1 - enter
+    // ---
+    // 0 - exit exitActive
+    // 1 - enter enterActive
+    // ---
+    // 0 -
+    // 1 - current
 
     const items = [
         {bg: '#2ecc71', title: 'slide 1'},
@@ -30,43 +33,102 @@ function TransitionSample1() {
         {bg: '#f1c40f', title: 'slide 5'},
     ];
 
+    const itemsRefs = useRef({})
+    const setRef = index => e => itemsRefs.current[index] = e
+
     const itemComps = items.map(({bg, title}, index) => {
-        const isEnter = index === transitionFlags['enter'];
-        const isExit = index === transitionFlags['exit'];
-        const isEntering = index === transitionFlags['enter-active'];
-        // const isExiting = index === transitionFlags['exit-active'];
-        const isDone = currentIndex && !isEnter
-        // const isBeforeEntering = currentIndex && isEnter && !isEntering
-        const shouldShow = index === isDone || isEntering || isExit
+        // const enter = index === transitionFlags.enter;
+        // const exit = index === transitionFlags.exit;
+        // const enterActive = index === transitionFlags.enterActive;
+        // const exitActive = index === transitionFlags.exitActive;
+        // // const isExiting = index === transitionFlags['exitActive'];
+        // const isDone = index === currentIndex && !enter
+        // // const isBeforeEntering = currentIndex && isEnter && !isEntering
+        // const shouldShow = isDone || enterActive || exit
 
         return (
-            <div key={`item-${index}`} className={classnames({item: true, hide: !shouldShow, enter: isEnter, exit: isExit})} style={{backgroundColor: bg}}><h1>{title}</h1></div>
+            <div key={`item-${index}`} ref={setRef(index)} className={classnames({item: true})} style={{backgroundColor: bg}}><h1>{title}</h1></div>
         )
     })
 
-    const nextSlide = () => {
+    useEffect(() => {
+        itemsRefs.current[0].classList.add("current");
+    }, [])
+
+    const prevIndex = useRef(-1)
+    useEffect(() => {
+        if (prevIndex.current !== -1) {
+            const prev = prevIndex.current
+            setTimeout(() => {
+                console.log(`prev ${prev} current ${currentIndex}`)
+                itemsRefs.current[prev].classList.remove("exit");
+                itemsRefs.current[prev].classList.remove("exitActive");
+                itemsRefs.current[prev].classList.remove("current");
+                itemsRefs.current[currentIndex].classList.remove("enter");
+                itemsRefs.current[currentIndex].classList.remove("enterActive");
+                itemsRefs.current[currentIndex].classList.add("current");
+            }, 3000)
+        }
+
+        prevIndex.current = currentIndex
+    }, [currentIndex])
+
+    const nextSlide1 = () => {
         const nextIndex = currentIndex < items.length - 1 ? currentIndex + 1 : 0
         setCurrentIndex(nextIndex)
         setTransitionFlags({
             enter: nextIndex,
             exit: currentIndex,
-            'enter-active': -1,
-            'exit-active': -1,
+            enterActive: -1,
+            exitActive: -1,
         })
     }
 
-    // if (!isUndefined(prevIndex) && prevIndex !== currentIndex) {
-    //     startTransition({enterIndex: currentIndex, exitIndex: prevIndex})
-    // }
+    const nextSlide2 = () => {
+        setTransitionFlags({
+            enter: transitionFlags.enter,
+            exit: transitionFlags.exit,
+            enterActive: transitionFlags.enter,
+            exitActive: transitionFlags.exit,
+        })
+    }
+
+    const end = () => {
+        setTransitionFlags({
+            enter: -1,
+            exit: -1,
+            enterActive: -1,
+            exitActive: -1,
+        })
+    }
+
+    const next1 = () => {
+        const nextIndex = currentIndex < items.length - 1 ? currentIndex + 1 : 0
+        itemsRefs.current[currentIndex].classList.add("exit");
+        itemsRefs.current[nextIndex].classList.add("enter");
+
+        const scrollTop = itemsRefs.current[currentIndex].scrollTop
+        itemsRefs.current[currentIndex].classList.add("exitActive");
+        itemsRefs.current[nextIndex].classList.add("enterActive");
+
+        setCurrentIndex(nextIndex)
+        // setCurrentIndex(nextIndex) // should observe to currentIndex changed and only then start changing classes
+        // setTransitionFlags({
+        //     enter: -1,
+        //     exit: -1,
+        //     enterActive: -1,
+        //     exitActive: -1,
+        // })
+    }
 
     return (
         <div>
             {/*<h1>{`prev: ${prevIndex}, curr: ${currentIndex}`}</h1>*/}
-            {/*<h1>{`flags: ${transitionFlags.enter}, ${transitionFlags.exit}, ${transitionFlags['enter-active']}, ${transitionFlags["exit-active"]}`}</h1>*/}
+            {/*<h1>{`flags: ${transitionFlags.enter}, ${transitionFlags.exit}, ${transitionFlags['enterActive']}, ${transitionFlags["exitActive"]}`}</h1>*/}
             <div className="items-container">
                 {itemComps}
             </div>
-            <input type="button" value="next" onClick={nextSlide}/>
+            <input type="button" value="next" onClick={next1}/>
         </div>
 
     );
